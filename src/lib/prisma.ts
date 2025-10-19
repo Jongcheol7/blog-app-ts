24fe031224cde0f0eaf00ddef18813506571d9d5
Lee JongCheol <whdcjf817@naver.com>
2025-10-19 21:04:52 +0900

    prisma 설정 수정

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -11,7 +11,11 @@ const globalForPrisma = globalThis as unknown as {
 // prisma가 이미 전역에 존재하면 그것을 사용하고, 없으면 새로 생성하는 코드이다.
 // a ?? b 문법은 a가 null이거나 undefinded 일때 b 사용
 // a || b 문법은 a가 falsy일때 즉 null,undefinded,false,0,'' 일때 b 사용
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error", "warn"], // ← 에러 추적용 (선택)
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
